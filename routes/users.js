d455ca6dbcd7c3f8d6354a3155bf2b89e1ad8285
ornel77 <t.ornel@yahoo.fr>
2020-01-13 18:39:45 +0100

    password encrypted and register route ok

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const bcrypt = require('bcryptjs')
 //to make sure that things are sent:
 const { check, validationResult } = require('express-validator/check')
 
@@ -17,7 +17,7 @@ router.post('/', [
     check('name', 'Please add name').not().isEmpty(),
     check('email','Please include a valid email').isEmail(),
     check('password','Please enter a ppasword with 6 or more characters').isLength({ min: 6})
-], (req,res) => {
+], async (req,res) => {
     //gives the data send to the route (email, name , pswd..)
     // res.send(req.body) 
     const errors = validationResult(req)
@@ -26,7 +26,33 @@ router.post('/', [
         return res.status(400).json({errors: errors.array()})
     }
 
-    res.send('passed')
+    const { name, email, password } = req.body
+
+    try {
+        let user = await User.findOne({ email })
+
+        if(user) {
+            return res.status(400).json({ msg: 'User already exists'})
+        }
+
+        //else we create a new user with User model
+        user = new User({
+            name,
+            email,
+            password
+        })
+
+        //encrypt password
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(password, salt)
+        await user.save()
+
+        res.send('User saved')
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server error')
+    }
 })
 
 module.exports = router
